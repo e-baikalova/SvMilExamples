744aa485582cd15c8236e1b59cd559df67af1a7c
Svetlana <111175@rambler.ru>
2024-01-08 12:19:01 +0300

    .\Homework 01.csproj

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+Console.WriteLine("Введите целое число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+if(number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.Write($"{number} → да ");
+}
+
+else
+{
+    System.Console.Write($"{number} → нет");
+}
